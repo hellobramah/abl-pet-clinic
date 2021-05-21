a35fbb001a8262cc8ac63059a26ac6b169c33e09
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-05-21 01:37:55 +0000

    Initial commit

---
+++ src/main/java/hellobramah/springframework/ablpetclinic/AblPetClinicApplication.java
@@ -0,0 +1,13 @@
+package hellobramah.springframework.ablpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AblPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AblPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hellobramah/springframework/ablpetclinic/AblPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package hellobramah.springframework.ablpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AblPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
