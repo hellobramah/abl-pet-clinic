89e0d0e8dafcc746c234fb4977bf3e6d0318e22a
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-06-06 22:25:40 +0000

    Implement Map Based Services. Closes #8

---
+++ pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package hellobramah.springframework.ablpetclinic.services.map;
+
+
+import hellobramah.springframework.ablpetclinic.model.Pet;
+import hellobramah.springframework.ablpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+}
---
+++ pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package hellobramah.springframework.ablpetclinic.services.map;
+
+import hellobramah.springframework.ablpetclinic.model.Vet;
+import hellobramah.springframework.ablpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+}
