a08d8eeeb73305a33fa8003483f928a2483d610c
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-06-09 19:33:33 +0000

    List all Vets on the Vets Index Page. Closes #15

--- pet-clinic-web/src/main/java/hellobramah/springframework/ablpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/hellobramah/springframework/ablpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package hellobramah.springframework.ablpetclinic.controllers;
 
+import hellobramah.springframework.ablpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+
         System.out.println("in vets controller");
         return "vets/index";
 
