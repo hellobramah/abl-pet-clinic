1948bb172860dfc485fa2ea9c2864f36413d693f
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-06-13 22:03:44 +0000

    Add Contact Info Properties to Owner. Closes #28

--- pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/model/Owner.java
@@ -1,20 +1,44 @@
 package hellobramah.springframework.ablpetclinic.model;
 
-import java.time.LocalDate;
 import java.util.Set;
 
 public class Owner extends Person
 {
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthDate;
+
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 
-    public Set<Pet> getPet() {
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
         return pets;
     }
 
-    public void setPet(Set<Pet> pets) {
+    public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
 }
