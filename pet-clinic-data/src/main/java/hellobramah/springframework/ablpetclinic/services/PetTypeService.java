13298e686d30a61d9fd8235705bfc3400997f0ac
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-06-13 23:17:03 +0000

    Create PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package hellobramah.springframework.ablpetclinic.services;
+
+import hellobramah.springframework.ablpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
