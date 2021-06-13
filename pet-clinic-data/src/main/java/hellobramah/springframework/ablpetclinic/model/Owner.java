441ef808daf211f3389329d0d7543f1e99fb89a5
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-06-13 21:47:40 +0000

    Create Pet, PetType and Visit Entities. Closes #26

--- pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/model/Owner.java
@@ -1,10 +1,20 @@
 package hellobramah.springframework.ablpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 public class Owner extends Person
 {
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
+    private Set<Pet> pets;
+
+    public Set<Pet> getPet() {
+        return pets;
+    }
+
+    public void setPet(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
