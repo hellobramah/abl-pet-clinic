cb6992e6e9ad287b566611a3a38be6187479a1f1
Alex Bramah-Lawani <hellobramah@gmail.com>
2021-06-13 21:56:17 +0000

    Create Vet Specialty Entity, Associate to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/hellobramah/springframework/ablpetclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package hellobramah.springframework.ablpetclinic.model;
+
+public class Specialty {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
